886ca003ac8b0f31775b344f0f76efc6f56bf51f
lennykamande <lennymanyeki@gmail.com>
2017-09-22 05:49:07 +0300

    Another Trial

--- Procfile
+++ Procfile
@@ -1,5 +1,2 @@
-export FLASH_CONFIG=development
-export FLASH_APP=run.py
-
 Web: gunicorn run:app
 
--- run.py
+++ run.py
@@ -2,7 +2,8 @@ import os
 
 from app import create_app
 
-config_name = os.getenv('FLASK_CONFIG', 'development')
+
+config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
 
 if __name__ == '__main__':
